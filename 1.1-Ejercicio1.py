e92b8b18ea0ddd7dc4dc61fc47897bbdc4f80b8d
deadmax-alanhernandez <alandavidhernandezvillalobos@gmail.com>
2026-03-23 16:56:51 -0400

    Add files via upload

---
+++ 1.1-Ejercicio1.py
@@ -0,0 +1,8 @@
+nombre_usuario = "Alan"
+edad = 17
+hobby = "me gusta leer y jugar juegos"
+
+print(nombre_usuario)
+print(edad)
+
+print(f"Hola, mucho gusto mi nombre es {nombre_usuario} y tengo {edad} , cuando tengo tiempo libre {hobby}")
---
+++ 1.2-ejercicio2.py
@@ -0,0 +1,7 @@
+print("Calculadora area de un triangulo")
+
+base = int(input("Valor de la base: "))
+altura = int(input("Valor de la altura: "))
+area = (base * altura)/2
+
+print(f"el valor de la area es {area} cm cuadrados")

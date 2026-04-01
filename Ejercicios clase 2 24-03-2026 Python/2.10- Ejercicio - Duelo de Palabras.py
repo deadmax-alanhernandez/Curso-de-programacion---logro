871dee6273d6a1232cf4398abcf5e566727c6b7d
deadmax-alanhernandez <alandavidhernandezvillalobos@gmail.com>
2026-04-01 16:49:02 -0400

    Commit

--- Ejercicios clase 2 24-03-2026 Python/2.10- Ejercicio - Duelo de Palabras.py
+++ Ejercicios clase 2 24-03-2026 Python/2.10- Ejercicio - Duelo de Palabras.py
@@ -3,7 +3,7 @@ print("¿Cual es mas larga?")
 palabra1 = str(input("ingrese palabra 1: "))
 palabra2 = str(input("ingrese palabra 2: "))
 
-largo = bool(palabra1 > palabra2)
+largo = len(palabra1) > len(palabra2)
 
 print(f"¿{palabra1} es mas larga que palabra {palabra2}?")
 print(largo)

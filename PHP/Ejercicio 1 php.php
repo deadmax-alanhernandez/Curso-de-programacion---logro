1daf055472930a900869d19354ad9995c3a285a5
deadmax-alanhernandez <alandavidhernandezvillalobos@gmail.com>
2026-04-06 17:04:16 -0400

    XD

---
+++ PHP/Ejercicio 1 php.php
@@ -0,0 +1,16 @@
+<?php
+$pares = 0;
+$impares = 0;
+
+for ($it = 1; $it<=50; $it++) {
+    
+   if ($it % 2 == 0){
+       $pares++;}
+   else
+   $impares++;
+}
+echo "Cantidad de numeros pares: ". $pares."\n";
+echo "Cantidad de numeros impares: ". $impares."\n";
+
+    
+?>
